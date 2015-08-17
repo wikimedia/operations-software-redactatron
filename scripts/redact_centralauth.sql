add97ca54aa6fd3b7177f03a7c305727c7d8e22e
jcrespo <jcrespo@wikimedia.org>
2015-08-17 21:16:06 +0200

    Redact new column on centralauth gu_cas_token
    Change-Id: I186b3c17aaa95293d600a4b0b205580dff29f395

--- scripts/redact_centralauth.sql
+++ scripts/redact_centralauth.sql
@@ -7,7 +7,8 @@ NEW.gu_salt = '',
 NEW.gu_password = '',
 NEW.gu_password_reset_key = '',
 NEW.gu_password_reset_expiration = '',
-NEW.gu_auth_token = '';
+NEW.gu_auth_token = '',
+NEW.gu_cas_token = 1;
 
 CREATE TRIGGER centralauth.globaluser_update
 BEFORE UPDATE ON centralauth.globaluser
@@ -18,6 +19,7 @@ NEW.gu_salt = '',
 NEW.gu_password = '',
 NEW.gu_password_reset_key = '',
 NEW.gu_password_reset_expiration = '',
-NEW.gu_auth_token = '';
+NEW.gu_auth_token = '',
+NEW.gu_cas_token = 1;
 
 -- then UPDATE all records the first time
